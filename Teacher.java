204f408e456f460d5948b09eff7cd9f267630083
kubik20083 <kubik20083@mail.ru>
2023-10-10 00:01:40 +0300

    teachers

--- Teacher.java
+++ Teacher.java
@@ -3,6 +3,42 @@ public class Teacher {
     private int id;
     private String firstName;
     private String lastName;
-    private String fatherName;
-    
+
+    public Teacher(int id, String firstName, String lastName, int age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+    }
+
+    public Teacher(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
 }
